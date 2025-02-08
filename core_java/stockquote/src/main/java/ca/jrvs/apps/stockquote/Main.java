ce1fbc7600664a3173667c63d24688382c4759fc
Mohand Kessi <Mohandkessi808@gmail.com>
2025-02-08 18:22:41 -0500

    Implemented JUnit tests for StockQuoteDao, using Mockito to mock dummy objects 5/5 success

--- core_java/stockquote/src/main/java/ca/jrvs/apps/stockquote/Main.java
+++ core_java/stockquote/src/main/java/ca/jrvs/apps/stockquote/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
   public static void main(String[] args) {
     Dotenv dotenv = Dotenv.load();
-    String symbol = "MSFT";
+    String symbol = "ENS";
     String apiKey = dotenv.get("API_KEY");
     try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/stock_quote", "postgres", "password")) {
       StockQuoteDao dao = new StockQuoteDao(connection);
