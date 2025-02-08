c8c2d7eeb8a35c2c63465d94ce9f21396ecca799
Mohand Kessi <Mohandkessi808@gmail.com>
2025-02-08 18:33:56 -0500

    Implemented Position Model, Dao and test class, used Junit5 and Mockito

--- core_java/stockquote/src/main/java/ca/jrvs/apps/stockquote/StockPosition.java
+++ core_java/stockquote/src/main/java/ca/jrvs/apps/stockquote/StockPosition.java
@@ -5,4 +5,27 @@ public class StockPosition {
   private int numOfShares;
   private double valuePaid;
 
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public int getNumOfShares() {
+    return numOfShares;
+  }
+
+  public void setNumOfShares(int numOfShares) {
+    this.numOfShares = numOfShares;
+  }
+
+  public double getValuePaid() {
+    return valuePaid;
+  }
+
+  public void setValuePaid(double valuePaid) {
+    this.valuePaid = valuePaid;
+  }
 }
