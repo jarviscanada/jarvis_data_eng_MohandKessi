b3c8a9aa315156ebf2418044ce8a522072bb77da
Mohand <Mohandkessi808@gmail.com>
2025-07-24 11:33:10 -0400

    added navbar

--- frontend/trading-ui/src/page/Dashboard/Dashboard.jsx
+++ frontend/trading-ui/src/page/Dashboard/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Dashboard.scss'
+// Import NavBar below
+import NavBar from '../../component/NavBar/NavBar'
 
 function Dashboard(props) {
   return (
-    <div className="dashboard">Dashboard</div>
+    <div className="dashboard">
+				{/* Include NavBar below */}
+        <NavBar />
+        <div className="dashboard-content">Dashboard Content</div>
+    </div>
   )
 }
 
